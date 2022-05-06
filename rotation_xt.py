51f6675f3d626543c372fed00545616e2b3be688
AngryKirC <kirillmurz@yandex.ru>
2022-05-06 16:56:02 +0500

    Added an explanation and reverse cipher mode.

--- rotation_xt.py
+++ rotation_xt.py
@@ -1,6 +1,11 @@
-def r(t, k):
+# Available key space is [0, 0xFFF]
+# this is because we are using 6 bits for each key part (32 characters in alphabet, 2^6)
+# making a grand total of 12 bits, or 2^12 possible key values
+def r(t, key, reverse=False):
     p1, p2 = (key & 63, (key & 4032) >> 6)
-    p1, p2 = (-p1, -p2)
+    if reverse:
+        p1, p2 = (-p1, -p2)
+    
     asc = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" + "abcdefghijklmnopqrstuvwxyz" + ".,_?!"
     r = ""
     for i in range(len(t)):
@@ -11,4 +16,7 @@ def r(t, k):
     return r
 
 text = "test_абырвалг"
-print(r(text, 32))
+text = r(text, 32)
+print(text)
+text = r(text, 32, True)
+print(text)
